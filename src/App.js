26b0753135831d1ef9f513614218bdc7eede662b
Jonathan Martinez <57384298+jonathan-martin-dev@users.noreply.github.com>
2022-01-26 15:45:08 -0800

    Creating Feedback Items List

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackItem from './components/FeedbackItem';
+import FeedbackData from './data/FeedbackData';
+
 
 const App = () => {
-
+  const [feedback, setFeedback] = useState(FeedbackData)
   return (
     <>
       <Header />
