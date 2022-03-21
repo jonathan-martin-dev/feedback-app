83f9aa1ae846d4910dc2b533636ce3ca00f74ac4
Jonathan Martinez <57384298+jonathan-martin-dev@users.noreply.github.com>
2022-03-21 12:00:00 -0700

    Make changes to feedback list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { useState } from 'react';
 import Header from './components/Header';
-import FeedbackItem from './components/FeedbackItem';
+import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
+import Card from './components/shared/Card';
 
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const deleteFeedback = (id) => {
+    if (window.confirm(`Are you sure you want to delete`)) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+
   return (
     <>
       <Header />
-      <FeedbackItem />
+      <FeedbackStats feedback={feedback} />
+      <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
     </>
     );
 };
