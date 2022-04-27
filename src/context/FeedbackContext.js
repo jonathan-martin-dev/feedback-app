aeb39505abe2a50dbdfc7e1efa71a16a76a3df27
Jonathan Martinez <57384298+jonathan-martin-dev@users.noreply.github.com>
2022-04-27 13:54:41 -0700

    create mock db using json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,33 +1,21 @@
 import React from 'react';
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import {v4 as uuidv4 } from 'uuid'
 
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: `This is feedback item 1`,
-            rating: 10
-        },
-        {
-            id: 2,
-            text: `This is feedback item 2`,
-            rating: 9
-        },
-        {
-            id: 3,
-            text: `This is feedback item 3`,
-            rating: 7
-        }
-    ])
+    const [feedback, setFeedback] = useState([])
 
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
+
+    useEffect(() =>{
+        console.log(123)
+    }, [])
 
     //Delete Feedback
     const deleteFeedback = (id) => {
