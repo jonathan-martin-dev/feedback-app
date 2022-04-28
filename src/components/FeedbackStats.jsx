e4b19f15585b7d377fd2aeb37228691b0397a76b
Jonathan Martinez <57384298+jonathan-martin-dev@users.noreply.github.com>
2022-04-27 19:43:12 -0700

    final changes

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,22 +1,21 @@
+import React from 'react'
 import { useContext } from 'react'
 import FeedbackContext from '../context/FeedbackContext'
+//Calculate ratings avg
 
-function FeedbackStats() {
-  const { feedback } = useContext(FeedbackContext)
+const FeedbackStats = () => {
+    const { feedback } = useContext(FeedbackContext)
 
-  // NOTE: simpler average calculation
-  // Calculate ratings avg
-  const average = Math.round(
-    feedback.reduce((acc, { rating }) => acc + rating, 0) / feedback.length
-  )
+    let average = feedback.reduce((acc, cur) => {
+        return acc + cur.rating
+    }, 0) / feedback.length
 
-  // average = average.toFixed(1).replace(/[.,]0$/, '')
-  // average = Math.round(average)
+    average = average.toFixed(1).replace(/[.,]0$/, '')
 
   return (
-    <div className='feedback-stats'>
-      <h4>{feedback.length} Reviews</h4>
-      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+    <div className='feedback-stats' style={{margin: `20px`}}>
+    <h4>{feedback.length} Reviews</h4>
+    <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
     </div>
   )
 }
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { createContext, useState, useEffect } from 'react';
-import {v4 as uuidv4 } from 'uuid'
-
 
 const FeedbackContext = createContext();
 
@@ -18,26 +16,36 @@ export const FeedbackProvider = ({ children }) => {
         fetchFeedback()
     }, [])
 
-    //Fetch
-    const fetchFeedback = async() => {
-        const response = await fetch(`/feedback?_sort=id&_order=desc`)
-        const data = response.json()
+    // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(`/feedback?_sort=id&_order=desc`)
+    const data = await response.json()
 
-        setFeedback(data)
-        setIsLoading(false)
-    }
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
     //Delete Feedback
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async(id) => {
         if (window.confirm(`Are you sure you want to delete`)) {
+          await fetch(`/feedback/${id}`, { method: 'DELETE' })
           setFeedback(feedback.filter((item) => item.id !== id))
         }
       }
       
       //Add Feedback
-      const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
+      const addFeedback = async (newFeedback) => {
+        const response = await fetch('/feedback', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newFeedback),
+        })
+    
+        const data = await response.json()
+    
+        setFeedback([data, ...feedback])
       }
 
       //Set item to be updated
@@ -48,10 +56,28 @@ export const FeedbackProvider = ({ children }) => {
           })
       }
 
-      const updateFeedback = (id, updItem) => {
-          setFeedback(feedback.map((item) => item.id === id ? {
-              ...item, ...updItem } : item))
-          } 
+      // Update feedback item
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem),
+    })
+
+    const data = await response.json()
+
+    // NOTE: no need to spread data and item
+    setFeedback(feedback.map((item) => (item.id === id ? data : item)))
+
+    // FIX: this fixes being able to add a feedback after editing
+    // credit to Jose https://www.udemy.com/course/react-front-to-back-2022/learn/lecture/29768200#questions/16462688
+    setFeedbackEdit({
+      item: {},
+      edit: false,
+    })
+  }
     
 
     return (
